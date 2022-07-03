dd795a708f6d05f80c1da5c73a895ec7cd523827
Bhavuk <bhavuksharma2202@gmail.com>
2022-07-03 12:41:41 +0530

    heapsort

--- 006_heapSort.c
+++ 006_heapSort.c
@@ -1,23 +1,29 @@
 // Implementation of a HEAP
-// A heap is basically is just an array with values order in a very specific way
+// A heap basically is just an array with values order in a very specific way
 // like a binray tree (heap is used to create priority queues)
-//
+
 // TO CALCULATE A PARENT OR CHILD USE BELOW CODE FOR 0 INDEXED ARRAY
 // PARENT = floor(i/2)
 // LEFT CHILD = (2i+1)
 // RIGHT CHILD = (2i+2)
-//
+
 // MAX-HEAP always follows the Max-heap property that is A[PARENT(i)] >= A[i]
 // MIN-HEAP always follows the Min-heap property that is A[PARENT(i)] <= A[i]
-//
+
 // MINIMUN AND MAXIMUM ELEMENTS IN A HEAP OF HEIGHT h is 2^h - 2^(h+1)-1
-// Internal nodes range : 0 to floor(n/2)-1
-// Leaf nodes range : floor(n/2) to n-1
+// non-leaf nodes range : 0 to floor(n/2)-1
+// leaf nodes range : floor(n/2) to n-1
+
+// most nodes at a certain level: ceil(n/2^(h+1)) h is height
+
 
 #include <stdio.h>
 #include "headers/tools.h"
 
-int HEAP_SIZE = 9;
+
+// Array -> Heap(using heapify) -> heapSort
+
+int HEAP_SIZE;
 
 // time complexity for this recursive approach is O(log(n)) and space complexity is O(log(n))
 void max_heapify(int *array, int node)
@@ -98,17 +104,38 @@ void MAX_HEAPIFY(int *array, int node)
     } while (node < (HEAP_SIZE>>1));
 }
 
-void heapSort(int *array, int size)
+// heapify can be used in bottom up manner to create a heap
+// worst time complexity for build_max_heap is O(nlog(n))
+// average time complexity for build_max_heap is O(n) (linear time)
+void build_max_heap(int *array, int size)
 {
-    for (int i = size-1; i >= 0; i--)
-    {
+    // LOOP INVARIANT: i+1, i+2 .... n are already root of a max-heap
+    for (int i = (size-1)>>1; i >= 0; i--)
         max_heapify(array, i);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void heapsort(int *array, int size)
+{
+    HEAP_SIZE = size;
+    build_max_heap(array, size);
+    for (int i = size-1; i >= 1; i--)
+    {
+        swap(array, &array[i]);
+        HEAP_SIZE -= 1;
+        max_heapify(array, 0);
     }
 }
 
 int main(void)
 {
     int array[] = {59, 10, 64, 82, 103, 45, 31, 92, 81, 61};
-    heapSort(array, 10);
+    heapsort(array, 10);
     printArray(array, 10);
 }
