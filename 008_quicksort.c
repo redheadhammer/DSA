216ef486af7b940b19d51cfade76aaba53c37f7b
Bhavuk <bhavuksharma2202@gmail.com>
2022-10-17 16:14:33 +0530

    qsort

--- 008_quicksort.c
+++ 008_quicksort.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 
 
+// it works by putting every element
+// at its right position from call
+// to partition function.
 void swap(int *a, int *b)
 {
     int tmp = *a;
@@ -10,21 +13,30 @@ void swap(int *a, int *b)
     *b = tmp;
 }
 
+// partition the array in a way so that every element
+// right of pivot element will be greater than pivot
+// and elements left of pivot will always be smaller
+// than pivot element.
 int partition(int *array, int start, int end)
 {
-    int fill = start-1;
+    // fill will keep track of the element greater than
+    // the pivot element so that it can be replaced later
+    int fill = start;
     int pivot = array[end-1];
+
+    // invariant: if i == fill -> array[i] < pivot
+    // if i > fill -> array[fill] > pivot
     for (int i = start; i < end-1; i++)
     {
         if (array[i] < pivot) 
         {
-            fill += 1;
             swap(&array[i], &array[fill]);
+            fill += 1;
         }
     }
-    swap(&array[fill+1], &array[end-1]);
+    swap(&array[fill], &array[end-1]);
 
-    return fill+1;
+    return fill;
 }
 
 void quicksort(int *array, int start, int end)
